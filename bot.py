476b47878da7e95a33557560d56e267ac7f08061
abdouhafez20 <abdou.hafez20@gmail.com>
2025-07-30 04:33:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -10,7 +10,9 @@ import logging
 import random
 
 # ✅ إعدادات البوت
-TOKEN = '7815578930:AAHpu5e-bjGTRcve80JQ5fbdGKexQRhQsz8'
+TOKEN = '
+          7815578930:AAHpu5e-bjGTRcve80JQ5fbdGKexQRhQsz8
+        '
 ADMIN_ID = 1049380446
 BOT_USERNAME = "@Assiut61framebot"
 
